e23104b74249a961f4d1be18ac838b0dfa379786
sslavov93 <sslavov93@gmail.com>
2016-11-14 08:42:45 +0200

    Git script updated - ssh setup.

--- git.sh
+++ git.sh
@@ -1,3 +1,7 @@
 git config --global user.name "sslavov93"
 git.config --global user.email "sslavov93@gmail.com"
 
+ssh-keygen -t rsa -b 4096 -C "sslavov93@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub

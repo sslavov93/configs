763e548b53ef94a266716081d6c2047e43cbec9f
Svet <sslavov93@gmail.com>
2016-12-28 02:40:47 +0200

    Updated IntelliJ IDEA Version

--- IJ.sh
+++ IJ.sh
@@ -1,9 +1,8 @@
-IJ_VERSION="ideaIC-2016.2.5"
+IJ_VERSION="ideaIC-2016.3.1"
 IJ_ARCHIVE="$IJ_VERSION.tar.gz"
 
 wget https://download.jetbrains.com/idea/$IJ_ARCHIVE
 
-mv $IJ_ARCHIVE ~/Work/Tools
-tar -zxvf ~/Work/Tools/$IJ_ARCHIVE
-rm ~/Work/Tools/*.tar.gz
-./~/Work/Tools/IJ_VERSION/bin/idea.sh
+mv $IJ_ARCHIVE ~/Tools/
+tar -zxvf ~/Tools/$IJ_ARCHIVE -C ~/Tools/
+rm ~/Tools/*.tar.gz

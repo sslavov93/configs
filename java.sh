a587c1a847b246cf44d02dc138a0d0dea3b70087
Svet <sslavov93@gmail.com>
2016-12-28 10:20:07 +0200

    Automated Oracle Java 8 License accept

--- java.sh
+++ java.sh
@@ -1,5 +1,5 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get -y update
-
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer
-sudo update-alternatives --config java
+# sudo update-alternatives --config java

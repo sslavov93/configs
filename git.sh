5edc90d74a3a642e807cce5f464c10e11a47a7b8
sslavov93 <sslavov93@gmail.com>
2017-03-29 15:34:58 +0300

    Added git GUI diff tool config

--- git.sh
+++ git.sh
@@ -1,5 +1,8 @@
 git config --global user.name "sslavov93"
 git config --global user.email "sslavov93@gmail.com"
+git config --global merge.tool meld
+git config --global diff.guitool meld
+git config --global mergetool.meld.path /usr/bin/meld
 
 ssh-keygen -t rsa -b 4096 -C "sslavov93@gmail.com"
 eval "$(ssh-agent -s)"
--- simple.sh
+++ simple.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install python3-pip htop rabbitvcs* maven skype unzip zip gparted zsh
+sudo apt-get -y install python3-pip htop maven skype unzip zip gparted zsh meld
 
 sudo pip3 install --upgrade pip
 sudo pip3 install nltk awscli virtualenv boto3 beautifulsoup4 Flask plotly

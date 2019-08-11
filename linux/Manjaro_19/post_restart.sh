3e7c0c9604d5b0be89996f87447bdc3cde0946cd
Svet <sslavov93@gmail.com>
2019-08-11 16:57:27 +0300

    Create post_restart.sh

---
+++ linux/Manjaro_19/post_restart.sh
@@ -0,0 +1,4 @@
+cp /home/$USER/configs/linux/Manjaro_19/zshrc /home/$USER/.zshrc
+cp /home/$USER/configs/linux/Manjaro_19/agnoster /home/$USER/configs/linux/Manjaro_19/.oh-my-zsh/themes/agnoster.zsh-theme
+
+sudo reboot

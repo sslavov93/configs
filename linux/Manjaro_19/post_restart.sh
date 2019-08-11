3231c2b1dbfe1acebc82fbe27c1bbce1d7fd967d
Svet <sslavov93@gmail.com>
2019-08-11 16:58:56 +0300

    Delete post_restart.sh

--- linux/Manjaro_19/post_restart.sh
+++
@@ -1,4 +0,0 @@
-cp /home/$USER/configs/linux/Manjaro_19/zshrc /home/$USER/.zshrc
-cp /home/$USER/configs/linux/Manjaro_19/agnoster /home/$USER/configs/linux/Manjaro_19/.oh-my-zsh/themes/agnoster.zsh-theme
-
-sudo reboot
